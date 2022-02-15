c1ec0b79377e9013aeb4fd6a44108e8513221620
Pier Carlo Cadoppi <piercarlo.cadoppi@mail.polimi.it>
2022-02-15 14:03:14 +0100

    formatting

--- smoothies/src/SmoothPermsSlow.hs
+++ smoothies/src/SmoothPermsSlow.hs
@@ -4,11 +4,13 @@ module SmoothPermsSlow
     ) where
 
 -- https://stackoverflow.com/questions/14379185/function-privacy-and-unit-testing-haskell
-import SmoothPermsSlow.Internal ( split, smooth )
+import           SmoothPermsSlow.Internal       ( smooth
+                                                , split
+                                                )
 
 perms :: [a] -> [[a]]
-perms []     = [[]]
-perms xs     = [v:p | (v, vs) <- split xs, p <- perms vs]
+perms [] = [[]]
+perms xs = [ v : p | (v, vs) <- split xs, p <- perms vs ]
 
 smoothPerms :: Int -> [Int] -> [[Int]]
 smoothPerms n xs = filter (smooth n) (perms xs)
--- smoothies/src/SmoothPermsSlow/Internal.hs
+++ smoothies/src/SmoothPermsSlow/Internal.hs
@@ -1,9 +1,9 @@
 module SmoothPermsSlow.Internal where
 
 split :: [a] -> [(a, [a])]
-split []     = []
-split (x:xs) = (x, xs) : [(y, x:ys) | (y, ys) <- split xs]
+split []       = []
+split (x : xs) = (x, xs) : [ (y, x : ys) | (y, ys) <- split xs ]
 
 smooth :: (Ord a, Num a) => a -> [a] -> Bool
-smooth n (x:y:ys) = abs (y - x) < n && smooth n (y:ys)
-smooth _ _        = True
+smooth n (x : y : ys) = abs (y - x) < n && smooth n (y : ys)
+smooth _ _            = True
