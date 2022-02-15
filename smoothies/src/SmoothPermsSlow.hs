9a8fd0b52dcd2f75bcd8705ed7c1011ef39e16b3
Pier Carlo Cadoppi <piercarlo.cadoppi@mail.polimi.it>
2022-02-15 14:57:55 +0100

    the haddock documentation. build with 'stack build haddock'

--- smoothies/src/SmoothPermsSlow.hs
+++ smoothies/src/SmoothPermsSlow.hs
@@ -1,16 +1,26 @@
 module SmoothPermsSlow
-    ( perms
-    , smoothPerms
-    ) where
+  ( perms
+  , smoothPerms
+  ) where
 
 -- https://stackoverflow.com/questions/14379185/function-privacy-and-unit-testing-haskell
 import           SmoothPermsSlow.Internal       ( smooth
                                                 , split
                                                 )
 
-perms :: [a] -> [[a]]
+{-| 'perms' generates permutations using 'split'
+-}
+perms
+  :: [a]    -- ^ the input list '[a]'
+  -> [[a]]  -- ^ the list of the permutations of '[a]'
 perms [] = [[]]
 perms xs = [ v : p | (v, vs) <- split xs, p <- perms vs ]
 
-smoothPerms :: Int -> [Int] -> [[Int]]
+
+{-| 'smoothPerms' filters out 'perms' that are not 'smooth'
+-}
+smoothPerms
+  :: Int      -- ^ the argument of 'smooth'
+  -> [Int]    -- ^ the argument of 'perms'
+  -> [[Int]]  -- ^ the smooth permutations
 smoothPerms n xs = filter (smooth n) (perms xs)
--- smoothies/src/SmoothPermsSlow/Internal.hs
+++ smoothies/src/SmoothPermsSlow/Internal.hs
@@ -1,9 +1,26 @@
 module SmoothPermsSlow.Internal where
 
-split :: [a] -> [(a, [a])]
+{-| 'split' generates splits between any element of the list and the rest of the list
+-}
+split
+  :: [a]        -- ^ input list '[a]'
+  -> [(a, [a])] {-^ output list of pairs, 
+    where the first element is the element that has been split from the list
+    and the second element is the rest of the list
+  -}
 split []       = []
 split (x : xs) = (x, xs) : [ (y, x : ys) | (y, ys) <- split xs ]
 
-smooth :: (Ord a, Num a) => a -> [a] -> Bool
+
+{-| 'smooth' calculates if the permutation is smooth
+
+  Given a list of integers xs and an integer d, 
+  a smooth permutation of xs with maximum distance d is a permutation 
+  in which the difference of any two consecutive elements is at less than d.
+-}
+smooth
+  :: Int    -- ^ The maximum distance d
+  -> [Int]  -- ^ the list of integers xs
+  -> Bool   -- ^ True if the permutation is smooth
 smooth n (x : y : ys) = abs (y - x) < n && smooth n (y : ys)
 smooth _ _            = True
