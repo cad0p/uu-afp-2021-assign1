4385f65313d59623f3fc4589cd520ba53c15e5de
Pier Carlo Cadoppi <piercarlo.cadoppi@mail.polimi.it>
2022-02-16 13:55:16 +0100

    splitLengthElems fails with "[([],[])]" as input

--- smoothies/test/SmoothPermsTest.hs
+++ smoothies/test/SmoothPermsTest.hs
@@ -8,5 +8,12 @@ import SmoothPermsSlow.Internal (split)
 splitLength :: [a] -> Bool
 splitLength xs = (length . split) xs == length xs
 
-splitLengthElems :: [a] -> Bool 
-splitLengthElems xs = all (\tpl -> length (fst tpl) + length (snd tpl) == length xs) (split xs) 
+-- splitLengthElems :: [a] -> Bool 
+-- splitLengthElems xs = all (map (\(a, [b]) -> a : b) (split xs)
+splitLengthElems
+  :: (Foldable t1, Foldable t2) => [(t1 a1, t2 a2)] -> Bool
+splitLengthElems xs = 
+  let 
+    splitLengthElemsRec n [] = True 
+    splitLengthElemsRec n (x : xs) = ((length (fst x) + length (snd x)) == n) && splitLengthElemsRec n xs
+  in splitLengthElemsRec (length xs) xs
