2c44f59ed414979a7055d7416f92acba4214b3f9
Pier Carlo Cadoppi <piercarlo.cadoppi@mail.polimi.it>
2022-02-16 15:49:48 +0100

    giving up on quickCheck on split [[],[]]

--- smoothies/test/SmoothPermsTest.hs
+++ smoothies/test/SmoothPermsTest.hs
@@ -11,13 +11,20 @@ import SmoothPermsSlow.Internal (split)
 splitLength :: [a] -> Bool
 splitLength xs = (length . split) xs == length xs
 
-{-| 'splitLengthElems' checks 
--}
-splitLengthElems
-  :: (Foldable t1, Foldable t2) => [(t1 a1, t2 a2)] -> Bool
-splitLengthElems xs = 
-  let 
-    splitLengthElemsRec _ [] = True 
-    splitLengthElemsRec n (y : ys) = ((length (fst y) + length (snd y)) == n) && splitLengthElemsRec n ys
-  in splitLengthElemsRec (length xs) (split xs)
 
+{-| 'splitLengthElems' checks if every pair has a combined length of the same length
+  of the original 'split' argument 'xs'
+-}
+splitLengthElems :: Foldable t0 => [t0 a0] -> Bool
+splitLengthElems xs = splitLengthElem (length xs) (split xs)
+
+
+splitLengthElem :: (Foldable t1, Foldable t2) => Int -> [(t1 a1, [t2 a2])] -> Bool
+splitLengthElem _ [] = True 
+splitLengthElem n ((_, []) : ys) = (1 == n) && splitLengthElem n ys
+-- doesn't work, type issue, moving onto a custom generator
+-- splitLengthElem n (([], y2) : ys) = ((1 + length y2) == n) && splitLengthElem n ys
+splitLengthElem n ((y1, y2) : ys) = ((length y1 + length y2) == n) && splitLengthElem n ys
+
+
+-- splitElems
