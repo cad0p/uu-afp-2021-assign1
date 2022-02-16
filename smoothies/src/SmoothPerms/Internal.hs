9469d2d1475676403abe58dba84bc2a4b3388e50
Pier Carlo Cadoppi <piercarlo.cadoppi@mail.polimi.it>
2022-02-16 22:50:54 +0100

    my data structure

--- smoothies/src/SmoothPerms/Internal.hs
+++ smoothies/src/SmoothPerms/Internal.hs
@@ -1,15 +1,18 @@
 module SmoothPerms.Internal where
 
-data PermTree a = Nil 
-                  | Leaf  { toPerm    :: a
-                          , perm      :: a }
-                  | Node  { toPerm    :: a
-                          , perm      :: a
-                          , children  :: [PermTree a]}
+data PermTree = Nil 
+                  | Leaf  { toPerm    :: [Int]
+                          , perm      :: [Int]      }
+                  | Node  { toPerm    :: [Int]
+                          , perm      :: [Int]
+                          , children  :: [PermTree] }
                           deriving Show
 
 newTree = do
   let n1 = Node { toPerm    = [1, 2]
                 , perm      = []
-                , children  = []}
+                , children  = [] }
   print n1
+  let n2 = Leaf { toPerm    = []
+                , perm = [2, 1] }
+  print n2
