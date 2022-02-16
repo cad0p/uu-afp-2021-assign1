d72d6894753768dce42eb96868362c10cac0a6a5
Pier Carlo Cadoppi <piercarlo.cadoppi@mail.polimi.it>
2022-02-16 21:09:43 +0100

    added tasty-quickcheck instead of just QuickCheck

--- smoothies/test/Spec.hs
+++ smoothies/test/Spec.hs
@@ -1,22 +1,41 @@
+import           Test.Tasty
+import           Test.Tasty.QuickCheck         as QC
+
 import           SmoothPermsTest
 
-import           Test.QuickCheck                ( maxSize
-                                                , quickCheck
-                                                , quickCheckWith
-                                                , stdArgs
-                                                )
-
 main :: IO ()
-main = do
-  quickCheck splitLength
-  quickCheck splitLengthElems
-  quickCheck splitElems
 
-  -- https://devtut.github.io/haskell/quickcheck.html#limiting-the-size-of-test-data
-  quickCheckWith (stdArgs { maxSize = 10 }) permsLength
-  quickCheckWith (stdArgs { maxSize = 10 }) permsLengthElems
-  quickCheckWith (stdArgs { maxSize = 10 }) permsElems
+
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [properties]
+
+properties :: TestTree
+properties = testGroup "Properties" [qcSplitProps, qcPermsProps]
+
+
+qcSplitProps :: TestTree
+qcSplitProps = testGroup
+  ("(checked by QuickCheck)\n" ++ "split")
+  [ QC.testProperty "splitLength" splitLength
+  , QC.testProperty "splitLengthElems" splitLengthElems
+  , QC.testProperty "splitElems" splitElems
+  ]
+
+qcPermsProps :: TestTree
+qcPermsProps = adjustOption
+  (const (QuickCheckMaxSize 10))
+  (testGroup
+    ("(checked by QuickCheck)\n" ++ "perms")
+    [ -- https://devtut.github.io/haskell/quickcheck.html#limiting-the-size-of-test-data
+      QC.testProperty "permsLength" permsLength
+    , QC.testProperty "permsLengthElems" permsLengthElems
+    , QC.testProperty "permsElems" permsElems
   -- quickCheckWith (stdArgs { maxSize = 10 }) permsUnique
-  quickCheckWith (stdArgs { maxSize = 10 }) smoothPermsAreSmooth
-  quickCheckWith (stdArgs { maxSize = 10 }) smoothPermsArePerms
-  quickCheckWith (stdArgs { maxSize = 10 }) smoothPermsLength
+  -- does not work because they can be unique
+    , QC.testProperty "smoothPermsAreSmooth" smoothPermsAreSmooth
+    , QC.testProperty "smoothPermsArePerms" smoothPermsArePerms
+    , QC.testProperty "smoothPermsLength" smoothPermsLength
+    ]
+  )
