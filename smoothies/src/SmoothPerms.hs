bd1e2d69c8f7789c4482d2bccd88cd504bdb25c3
Pier Carlo Cadoppi <piercarlo.cadoppi@mail.polimi.it>
2022-02-18 03:28:50 +0100

    smoothperms now is not a placeholder but still fails a test case which is good it means the testing is actually useful!

--- smoothies/src/SmoothPerms.hs
+++ smoothies/src/SmoothPerms.hs
@@ -1,9 +1,18 @@
 module SmoothPerms (perms, smoothPerms) where
 
-import SmoothPerms.Internal (permTreeToPerms , listToPermTree)
+import SmoothPerms.Internal (  permTreeToPerms
+                             , listToPermTree
+                             , listToSmoothPermTree )
 
-smoothPerms :: Int -> [Int] -> [[Int]]
-smoothPerms = replicate
+{-| 'smoothPerms' generates smooth permutations
+  only expanding the 'PermTree' if the possible permutations
+  can be smooth (it does not expand all the leaves)
+-}
+smoothPerms 
+  :: Int      -- ^ the argument of 'smooth'
+  -> [Int]    -- ^ the argument of 'perms'
+  -> [[Int]]  -- ^ the list of the smooth permutations
+smoothPerms n xs = permTreeToPerms(listToSmoothPermTree n xs)
 
 {-| 'perms' generates permutations using 'PermTree'
 -}
